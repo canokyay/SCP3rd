d6e21ce0238ea04d17b02b3cf44c1cb5c50bac3e
canokyay <sabricanokyay_2@hotmail.com>
2021-04-12 19:08:17 +0300

    No such file fix try1

--- SCP2nd/spiders/a0makes1models.py
+++ SCP2nd/spiders/a0makes1models.py
@@ -5,21 +5,22 @@ import w3lib.html
 class A0makes1modelsSpider(scrapy.Spider):
     name = '0makes1models'
     allowed_domains = ['automobile-catalog.com']
-    with open('data/0makes.csv', "r") as f:
-        reader = csv.DictReader(f)
-        start_urls = []
-        for item in reader:
-            ### OFFLINE
-            start_urls.append(f"file:F:\GDrive\WorkSpace\Python\ScrapyProject1\ACpages\\1_ModelsList\\" + item['Make_Link'].replace("/", ""))
-            ### ONLINE
-            # if item['Available']: start_urls.append(item['Live_Link'])
 
-    # start_urls = [
-    #     f"file:F:\GDrive\WorkSpace\Python\ScrapyProject1\ACpages\\1_ModelsList\Alfa_Romeo_specifications_catalogue.html",
-    #     "https://www.automobile-catalog.com/list-jaguar.html"
-    #     "https://httpbin.org/ip"
-    # ]
-    print(start_urls)
+    # with open('data/0makes.csv', "r") as f:
+    #     reader = csv.DictReader(f)
+    #     start_urls = []
+    #     for item in reader:
+    #         ### OFFLINE
+    #         start_urls.append(f"file:F:\GDrive\WorkSpace\Python\ScrapyProject1\ACpages\\1_ModelsList\\" + item['Make_Link'].replace("/", ""))
+    #         ### ONLINE
+    #         # if item['Available']: start_urls.append(item['Live_Link'])
+
+    start_urls = [
+        # f"file:F:\GDrive\WorkSpace\Python\ScrapyProject1\ACpages\\1_ModelsList\Alfa_Romeo_specifications_catalogue.html",
+        "https://www.automobile-catalog.com/list-jaguar.html"
+        # "https://httpbin.org/ip"
+    ]
+    # print(start_urls)
 
     def parse(self, response):
         Make = response.xpath('//b[contains(., "Specifications catalogue of the")]/text()').get().replace("Specifications catalogue of the ","").partition(" cars,")[0]
